1a3f262e6c1db81cad63831de419ee1633fb8b53
justmyr92 <20-02126@g.batstate-u.edu.ph>
2023-07-11 12:28:50 +0800

    updated

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,36 +1,40 @@
 /** @format */
 
-import {Component, OnInit} from '@angular/core';
-import {DataService} from '../services/data.service';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.page.html',
-    styleUrls: ['./login.page.scss'],
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-    isIncorrect: boolean = false;
-    errorMessage: string = '';
+  isIncorrect: boolean = false;
+  errorMessage: string = '';
 
-    email: string = '';
-    password: string = '';
+  email: string = '';
+  password: string = '';
 
-    constructor(private dataService: DataService, private router: Router) {}
+  constructor(private dataService: DataService, private router: Router) {}
 
-    ngOnInit() {}
-
-    login() {
-        if (this.email != '' && this.password != '') {
-            if (this.dataService.login(this.email, this.password)) {
-                this.router.navigate(['/tabs/tab1']);
-            } else {
-                this.isIncorrect = true;
-                this.errorMessage = 'Incorrect email address and password';
-            }
-        } else {
-            this.errorMessage = 'Fill out all missing values.';
-            this.isIncorrect = true;
-        }
+  ngOnInit() {
+    if (localStorage.getItem('userId') != null) {
+      this.router.navigate(['/tabs/tab1']);
     }
+  }
+
+  login() {
+    if (this.email != '' && this.password != '') {
+      if (this.dataService.login(this.email, this.password)) {
+        this.router.navigate(['/tabs/tab1']);
+      } else {
+        this.isIncorrect = true;
+        this.errorMessage = 'Incorrect email address and password';
+      }
+    } else {
+      this.errorMessage = 'Fill out all missing values.';
+      this.isIncorrect = true;
+    }
+  }
 }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
