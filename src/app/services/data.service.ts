f93d54ba7559fcfd7e7caab4e0317e658b19ad7f
ericajane16 <20-49929@g.batstate-u.edu.ph>
2023-07-12 12:36:02 +0800

    with functional chart, but no specification of date

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,137 +1,147 @@
 /** @format */
-import {Injectable, EventEmitter} from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class DataService {
-    public userData: any = [
+  public userData: any = [
+    {
+      userId: 10001,
+      emailAddress: 'justmyrgutierrez1@gmail.com',
+      password: '123456',
+      participated: true,
+      solve: [
         {
-            userId: 10001,
-            emailAddress: 'justmyrgutierrez1@gmail.com',
-            password: '123456',
-            participated: true,
-            solve: [
-                {
-                    category: "3x3x3 Rubik's Cube",
-                    time: '10.00',
-                    scramble: "R U R' U'",
-                    date: '2021-01-01',
-                },
-                {
-                    category: "3x3x3 Rubik's Cube",
-                    time: '15.00',
-                    scramble: "R U R' U'",
-                    date: '2021-01-02',
-                },
-                {
-                    category: "3x3x3 Rubik's Cube",
-                    time: '20.00',
-                    scramble: "R U R' U'",
-                    date: '2021-01-03',
-                },
-            ],
+          category: "3x3x3 Rubik's Cube",
+          time: '10.00',
+          scramble: "R U R' U'",
+          date: '2023-07-10T04:13:45.852Z',
         },
         {
-            userId: 10002,
-            emailAddress: 'raizhellannquijano@gmail.com',
-            password: '123456',
-            participated: true,
-            solve: [
-                {
-                    category: "3x3x3 Rubik's Cube",
-                    time: '10.00',
-                    scramble: "R U R' U'",
-                    date: '2021-01-01',
-                },
-            ],
+          category: "3x3x3 Rubik's Cube",
+          time: '15.00',
+          scramble: "R U R' U'",
+          date: '2023-07-11T04:13:45.852Z',
         },
-    ];
+        {
+          category: "3x3x3 Rubik's Cube",
+          time: '20.00',
+          scramble: "R U R' U'",
+          date: '2023-07-12T04:13:45.852Z',
+        },
+      ],
+    },
+    {
+      userId: 10002,
+      emailAddress: 'raizhellannquijano@gmail.com',
+      password: '123456',
+      participated: true,
+      solve: [
+        {
+          category: "3x3x3 Rubik's Cube",
+          time: '10.00',
+          scramble: "R U R' U'",
+          date: '2023-07-12T04:13:45.852Z',
+        },
+      ],
+    },
+  ];
 
-    solvesChange: EventEmitter<any[]> = new EventEmitter<any[]>();
+  solvesChange: EventEmitter<any[]> = new EventEmitter<any[]>();
 
-    constructor() {}
+  constructor() {}
 
-    checkEmail(email: string) {
-        for (let i = 0; i < this.userData.length; i++) {
-            if (this.userData[i].emailAddress == email) {
-                return true;
-            }
+  checkEmail(email: string) {
+    for (let i = 0; i < this.userData.length; i++) {
+      if (this.userData[i].emailAddress == email) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addUser(email: string, password: string, participated: boolean) {
+    let userId = Math.floor(Math.random() * 90000) + 10000;
+    while (this.checkUserId(userId)) {
+      userId = Math.floor(Math.random() * 90000) + 10000;
+    }
+
+    let newUser = {
+      userId: userId,
+      emailAddress: email,
+      password: password,
+      participated: participated,
+      solve: [],
+    };
+
+    this.userData.push(newUser);
+  }
+
+  checkUserId(userId: number) {
+    for (let i = 0; i < this.userData.length; i++) {
+      if (this.userData[i].userId == userId) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  login(email: string, password: string) {
+    for (let i = 0; i < this.userData.length; i++) {
+      if (
+        this.userData[i].emailAddress == email &&
+        this.userData[i].password == password
+      ) {
+        localStorage.setItem('userId', this.userData[i].userId);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  setSolve(userId: number, solve: any) {
+    for (let i = 0; i < this.userData.length; i++) {
+      if (this.userData[i].userId == userId) {
+        this.userData[i].solve.push(solve);
+        this.solvesChange.emit(this.userData[i].solve);
+      }
+    }
+  }
+
+  getSolve(userId: number) {
+    for (let i = 0; i < this.userData.length; i++) {
+      if (this.userData[i].userId == userId) {
+        return this.userData[i].solve;
+      }
+    }
+  }
+
+  getSolveByCategory(userId: number, category: string) {
+    let solves = [];
+    for (let i = 0; i < this.userData.length; i++) {
+      if (this.userData[i].userId == userId) {
+        for (let j = 0; j < this.userData[i].solve.length; j++) {
+          if (this.userData[i].solve[j].category == category) {
+            solves.push(this.userData[i].solve[j]);
+          }
         }
-        return false;
+      }
     }
+    return solves;
+  }
 
-    addUser(email: string, password: string, participated: boolean) {
-        let userId = Math.floor(Math.random() * 90000) + 10000;
-        while (this.checkUserId(userId)) {
-            userId = Math.floor(Math.random() * 90000) + 10000;
-        }
-
-        let newUser = {
-            userId: userId,
-            emailAddress: email,
-            password: password,
-            participated: participated,
-            solve: [],
-        };
-
-        this.userData.push(newUser);
+  getSolves(userId: number) {
+    let solves = [];
+    for (let i = 0; i < this.userData.length; i++) {
+      if (this.userData[i].userId == userId) {
+        solves.push(this.userData[i].solve);
+      }
     }
+    return solves;
+  }
 
-    checkUserId(userId: number) {
-        for (let i = 0; i < this.userData.length; i++) {
-            if (this.userData[i].userId == userId) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    login(email: string, password: string) {
-        for (let i = 0; i < this.userData.length; i++) {
-            if (
-                this.userData[i].emailAddress == email &&
-                this.userData[i].password == password
-            ) {
-                localStorage.setItem('userId', this.userData[i].userId);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    setSolve(userId: number, solve: any) {
-        for (let i = 0; i < this.userData.length; i++) {
-            if (this.userData[i].userId == userId) {
-                this.userData[i].solve.push(solve);
-                this.solvesChange.emit(this.userData[i].solve);
-            }
-        }
-    }
-
-    getSolve(userId: number) {
-        for (let i = 0; i < this.userData.length; i++) {
-            if (this.userData[i].userId == userId) {
-                return this.userData[i].solve;
-            }
-        }
-    }
-
-    getSolveByCategory(userId: number, category: string) {
-        let solves = [];
-        for (let i = 0; i < this.userData.length; i++) {
-            if (this.userData[i].userId == userId) {
-                for (let j = 0; j < this.userData[i].solve.length; j++) {
-                    if (this.userData[i].solve[j].category == category) {
-                        solves.push(this.userData[i].solve[j]);
-                    }
-                }
-            }
-        }
-        return solves;
-    }
-
-    logoutUser() {
-        localStorage.clear();
-    }
+  logoutUser() {
+    localStorage.clear();
+  }
 }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,9 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import {
+  Chart,
+  LinearScale,
+  LineController,
+  CategoryScale,
+  PointElement,
+  LineElement,
+} from 'chart.js';
+import { DataService } from '../services/data.service';
+
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
+  constructor(private dataService: DataService) {}
+
+  labels: any[] = [];
+  solveTimes: number[] = [];
+  solveDates: string[] = [];
+
   ngOnInit() {}
+
+  ionViewWillEnter() {
+    this.renderChart();
+  }
+
+  renderChart() {
+    this.solveTimes = [];
+    this.solveDates = [];
+    let user = localStorage.getItem('userId');
+
+    let solvesArray = this.dataService.getSolves(Number(user));
+    for (const solves of solvesArray) {
+      for (const solve of solves) {
+        this.solveTimes.push(parseFloat(solve.time));
+        const date = new Date(solve.date);
+        const month = date.toLocaleDateString('en-US', { month: 'short' });
+        const day = date.getDate();
+        const formattedDate = `${month} ${day}`;
+        this.solveDates.push(formattedDate);
+      }
+    }
+    Chart.register(
+      LinearScale,
+      LineController,
+      CategoryScale,
+      PointElement,
+      LineElement
+    );
+    const canvas = document.getElementById('lineChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      Chart.getChart(ctx)?.destroy();
+
+      const lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: this.solveDates,
+          datasets: [
+            {
+              label: 'Solve Times',
+              data: this.solveTimes,
+              borderColor: 'blue',
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    }
+  }
 }
